2c0c67b79348eccdda412224f7b4110a897fbc05
charles-louis <contactokaou@gmail.com>
2015-06-16 03:05:11 +0200

    adds url and single quoteds

--- src/app/projects/services/projects-service.js
+++ src/app/projects/services/projects-service.js
@@ -8,73 +8,95 @@
  */
 angular.module('webdesignProjects2015')
   .service('projectsService', function () {
-// AngularJS will instantiate a singleton by calling "new" on this function
+// AngularJS will instantiate a singleton by calling 'new' on this function
     this.projects = [
 		{
-			"id": "B00309675",
-			"title": "B00309675",
-			"description": "B00309675",
-			"logo": "yeoman.png"
+			'id': 'B00309675',
+			'name': 'Prénom NOM',
+			'title': 'B00309675',
+			'description': 'B00309675',
+			'logo': 'yeoman.png',
+			'url': 'projects/B00309675'
 		},
 		{
-			"id": "B00342015",
-			"title": "B00342015",
-			"description": "B00342015",
-			"logo": "yeoman.png"
+			'id': 'B00342015',
+			'name': 'Prénom NOM',
+			'title': 'B00342015',
+			'description': 'B00342015',
+			'logo': 'yeoman.png',
+			'url': 'projects/B00342015'
 		},
 		{
-			"id": "B00437942",
-			"title": "B00437942",
-			"description": "B00437942",
-			"logo": "yeoman.png"
+			'id': 'B00437942',
+			'name': 'Prénom NOM',
+			'title': 'B00437942',
+			'description': 'B00437942',
+			'logo': 'yeoman.png',
+			'url': 'projects/B00437942'
 		},
 		{
-			"id": "B00462613",
-			"title": "B00462613",
-			"description": "B00462613",
-			"logo": "yeoman.png"
+			'id': 'B00462613',
+			'name': 'Prénom NOM',
+			'title': 'B00462613',
+			'description': 'B00462613',
+			'logo': 'yeoman.png',
+			'url': 'projects/B00462613'
 		},
 		{
-			"id": "B00470439",
-			"title": "B00470439",
-			"description": "B00470439",
-			"logo": "yeoman.png"
+			'id': 'B00470439',
+			'name': 'Prénom NOM',
+			'title': 'B00470439',
+			'description': 'B00470439',
+			'logo': 'yeoman.png',
+			'url': 'projects/B00470439'
 		},
 		{
-			"id": "B00496963",
-			"title": "B00496963",
-			"description": "B00496963",
-			"logo": "yeoman.png"
+			'id': 'B00496963',
+			'name': 'Prénom NOM',
+			'title': 'B00496963',
+			'description': 'B00496963',
+			'logo': 'yeoman.png',
+			'url': 'projects/B00496963'
 		},
 		{
-			"id": "B00497716",
-			"title": "B00497716",
-			"description": "B00497716",
-			"logo": "yeoman.png"
+			'id': 'B00497716',
+			'name': 'Prénom NOM',
+			'title': 'B00497716',
+			'description': 'B00497716',
+			'logo': 'yeoman.png',
+			'url': 'projects/B00497716'
 		},
 		{
-			"id": "B00514339",
-			"title": "B00514339",
-			"description": "B00514339",
-			"logo": "yeoman.png"
+			'id': 'B00514339',
+			'name': 'Prénom NOM',
+			'title': 'B00514339',
+			'description': 'B00514339',
+			'logo': 'yeoman.png',
+			'url': 'projects/B00514339'
 		},
 		{
-			"id": "B00522473",
-			"title": "B00522473",
-			"description": "B00522473",
-			"logo": "yeoman.png"
+			'id': 'B00522473',
+			'name': 'Prénom NOM',
+			'title': 'B00522473',
+			'description': 'B00522473',
+			'logo': 'yeoman.png',
+			'url': 'projects/B00522473'
 		},
 		{
-			"id": "B00571183",
-			"title": "B00571183",
-			"description": "B00571183",
-			"logo": "yeoman.png"
+			'id': 'B00571183',
+			'name': 'Prénom NOM',
+			'title': 'B00571183',
+			'description': 'B00571183',
+			'logo': 'yeoman.png',
+			'url': 'projects/B00571183'
 		},
 		{
-			"id": "B00573117",
-			"title": "B00573117",
-			"description": "B00573117",
-			"logo": "yeoman.png"
+			'id': 'B00573117',
+			'name': 'Prénom NOM',
+			'title': 'B00573117',
+			'description': 'B00573117',
+			'logo': 'yeoman.png',
+			'url': 'projects/B00573117'
 		}
     ];
   });
